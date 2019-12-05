fed25115e037844833ac9c2cf319d7a8bc44c7f7
Luiz Vianna <luizvianna02@outlook.com>
2019-12-05 01:04:06 -0300

    Tratando 404

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Categoria, Anuncio
 
 def home(request):
     categorias = Categoria.objects.all()
     ultimos_anuncios = Anuncio.objects.all()[:12]
+    count_anuncios = []
+    for c in range(categorias.count()):
+        count_anuncios.append(Anuncio.objects.filter(categoria=categorias[c]).count())
 
     return render(request, 'home.html', {'categorias': categorias,
-                                         'anuncios': ultimos_anuncios})
+                                         'anuncios': ultimos_anuncios,
+                                         'count_anuncios': count_anuncios,
+                                         })
 
 
 def categoria(request, slug):
     categorias = Categoria.objects.all()
-    categoria_slug = Categoria.objects.filter(slug=slug).get()
+
+    categoria_slug = get_object_or_404(Categoria, slug=slug)#Categoria.objects.filter(slug=slug).get()
 
     anuncios = Anuncio.objects.filter(categoria=categoria_slug)
+    count_anuncios = []
+    for c in range(categorias.count()):
+        count_anuncios.append(Anuncio.objects.filter(categoria=categorias[c]).count())
 
     return render(request, 'home.html', {'categorias': categorias,
-                                         'anuncios': anuncios})
+                                         'anuncios': anuncios,
+                                         'count_anuncios': count_anuncios,})
 # Create your views here.
